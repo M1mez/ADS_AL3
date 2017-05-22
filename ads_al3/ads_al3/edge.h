ba3d14e29bb6002accc3e04877faa0b7ab317d3f
M1mez <Fpscorner@gmail.com>
2017-05-22 10:32:11 +0200

    readfile angepasst

--- ads_al3/ads_al3/edge.h
+++ ads_al3/ads_al3/edge.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "vertex.h"
+#include <iostream>
+#include <string>
 
 struct Edge
 {
--- ads_al3/ads_al3/manager.cpp
+++ ads_al3/ads_al3/manager.cpp
@@ -1,6 +1,8 @@
 #include "manager.h"
+#include <fstream>
+#include <sstream>
 
-
+using namespace std;
 
 Manager::Manager()
 {
@@ -14,6 +16,49 @@ Manager::~Manager()
 
 void Manager::readFile()
 {
+	string tmp, line, name, station, path, devNull;
+	int a, b;
+	a = b = 0;
+
+	path = "../../ubahn.txt";
+
+	ifstream ifs(path.c_str());
+
+
+	getline(ifs, line, '\n');
+	stringstream ss(line);
+
+	getline(ss, name, ':');
+	getline(ss, devNull, '"');
+	getline(ss, station, '"');
+
+		if (m_stations.count(station))
+		{
+
+		}
+		else
+		{
+			m_stations[name] = new Vertex(name);
+		}
+
+
+	if (ifs.good()) {
+		
+		m_labString += (tmp + '\n');
+		m_height++;
+
+		m_width = tmp.size();
+
+		while (!ifs.eof()) {
+			getline(ifs, tmp, '\n');
+			if (tmp.size() == m_width)
+			{
+				m_labString += (tmp + '\n');
+				m_height++;
+			}
+		}
+		m_width++;
+	}
 
 }
 
@@ -24,5 +69,12 @@ Vertex* Manager::getStation(std::string name)
 
 void Manager::newStation(std::string name)
 {
-	m_stations[name] = new Vertex(name);
+	if (m_stations.count(name))
+	{
+
+	}
+	else
+	{
+		m_stations[name] = new Vertex(name);
+	}
 }
